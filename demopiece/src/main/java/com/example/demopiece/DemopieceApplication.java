485689a4b28bb4468d39817f16cf6be74b8785a8
SanjithBalaji <113413000+SanjithBalaji@users.noreply.github.com>
2023-04-20 12:03:23 +0530

    Demopiece
    Day 1 Project

---
+++ demopiece/src/main/java/com/example/demopiece/DemopieceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demopiece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemopieceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemopieceApplication.class, args);
+	}
+
+}
---
+++ demopiece/src/test/java/com/example/demopiece/DemopieceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demopiece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemopieceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
